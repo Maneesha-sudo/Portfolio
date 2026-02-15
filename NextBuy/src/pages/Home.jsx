c8e1821d11beddaa2d7f5b0a45d052713f7e1fbc
maneesha-sudo <matetimaneesha4@gmail.com>
2026-02-15 20:31:38 +0530

    made changes

--- NextBuy/src/pages/Home.jsx
+++ NextBuy/src/pages/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
     <div className="min-h-screen bg-gray-100">
 
 
+
       {/* Content Section */}
       <div className="flex flex-col items-center text-center px-6 py-12">
         <h1 className="text-4xl md:text-5xl font-bold mb-6">
@@ -15,11 +16,15 @@ function Home() {
           Discover high-quality products at unbeatable prices.
           Shop smart, shop easy.
         </p>
-        {/* Hero Image */}
-        <img
-          src="https://media.istockphoto.com/id/1157459058/photo/young-woman-with-shopping-bags-riding-trolley.jpg?s=612x612&w=0&k=20&c=nTjWjCed_gUe0OnpHG3IlCOc6QqYDTdXkf3Asu0RtNY="
-          alt="Shopping"
-          className="w-full h-[400px] object-cover" />
+        {/* Hero Section */}
+        <div className="w-full">
+          <img
+            src="https://media.istockphoto.com/id/1157459058/photo/young-woman-with-shopping-bags-riding-trolley.jpg?s=612x612&w=0&k=20&c=nTjWjCed_gUe0OnpHG3IlCOc6QqYDTdXkf3Asu0RtNY="
+            alt="Shopping"
+            className="w-full object-contain"
+          />
+        </div>
+
         <Link
           to="/products"
           className="mt-3 bg-blue-600 text-white px-6 py-3 rounded hover:bg-blue-700 transition"
